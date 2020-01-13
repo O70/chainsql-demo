1ce733926777a974257501fb85dff204a66dc4bc
鬼王 <THRAEX@aliyun.com>
2020-01-13 15:34:57 +0800

    style: update 404

--- client/src/views/404.vue
+++ client/src/views/404.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    Page Not Found - 404
+  <div class="not-found">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Page Not Found - 404</h1>
   </div>
 </template>
 <script>
@@ -8,3 +9,13 @@ export default {
   name: 'Page404'
 }
 </script>
+<style lang="scss" scoped>
+.not-found {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
