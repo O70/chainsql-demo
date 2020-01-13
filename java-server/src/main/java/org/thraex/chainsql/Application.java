be52614584360e84b9a8e7d99cc9039e08df14aa
鬼王 <THRAEX@aliyun.com>
2020-01-13 14:29:20 +0800

    chore: initial project of java server

---
+++ java-server/src/main/java/org/thraex/chainsql/Application.java
@@ -0,0 +1,17 @@
+package org.thraex.chainsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 鬼王
+ * @date 2020/01/13 11:46
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
